c5fea4b7ea11de8bb07cf9041e8a1705c56e2abc
Padmavathi Manoharan <padmam725@gmail.com>
2023-12-01 12:44:45 -0500

    icon align change

--- src/components/orderbookcomponent.jsx
+++ src/components/orderbookcomponent.jsx
@@ -9,13 +9,13 @@ const OrderBookComponent = (props) => {
     return <p>Loading...</p>;
   }
 
-  let topPrice, takerSide;
+  let topPrice, takerColor;
   const RecentTradesComponent = (props) => {
     const { recentTrade } = props;
     const reversedTrades = recentTrade ? [...recentTrade].reverse() : [];
     // Get the top (1) price
     topPrice = reversedTrades.length > 0 ? reversedTrades[0].price : null;
-    takerSide = reversedTrades.length > 0 ? reversedTrades[0].takerSide : null;
+    takerColor = reversedTrades.length > 0 ? reversedTrades[0].takerSide : null;
     return topPrice;
   };
 
@@ -67,7 +67,7 @@ const OrderBookComponent = (props) => {
                   fontSize: "2.2em",
                   textAlign: "center",
                 }}
-                className={takerSide === "B" ? "color-red" : "color-green"}
+                className={takerColor === "B" ? "color-red" : "color-green"}
               >
                 <RecentTradesComponent {...props} topPrice={topPrice} />
                 <svg
@@ -76,6 +76,7 @@ const OrderBookComponent = (props) => {
                   height="1em"
                   width="1em"
                   {...props}
+                  style={{ verticalAlign: "middle" }}
                 >
                   <path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z" />
                 </svg>
