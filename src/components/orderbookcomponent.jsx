9acb81a646643714265859fd03d4351360dbaf53
Padmavathi Manoharan <padmam725@gmail.com>
2023-12-01 11:57:33 -0500

    orderbook totalprice display changes

--- src/components/orderbookcomponent.jsx
+++ src/components/orderbookcomponent.jsx
@@ -59,21 +59,27 @@ const OrderBookComponent = (props) => {
                 <td>{askCumulativeTotal[index]}</td>
               </tr>
             ))}
-            <tr
-              colSpan="3"
-              style={{ fontWeight: "bold", fontSize: "1.2em" }}
-              className={takerSide === "B" ? "color-red" : "color-green"}
-            >
-              <RecentTradesComponent {...props} topPrice={topPrice} />
-              <svg
-                viewBox="0 0 1024 1024"
-                fill="currentColor"
-                height="1em"
-                width="1em"
-                {...props}
+            <tr>
+              <td
+                colSpan="3"
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "2.2em",
+                  textAlign: "center",
+                }}
+                className={takerSide === "B" ? "color-red" : "color-green"}
               >
-                <path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z" />
-              </svg>
+                <RecentTradesComponent {...props} topPrice={topPrice} />
+                <svg
+                  viewBox="0 0 1024 1024"
+                  fill="currentColor"
+                  height="1em"
+                  width="1em"
+                  {...props}
+                >
+                  <path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z" />
+                </svg>
+              </td>
             </tr>
             {bidOrderBookSlice.map(([price, size], index) => (
               <tr key={index} className="color-green">
